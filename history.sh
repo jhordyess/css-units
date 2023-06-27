d9ac693d224512db3bb58509f67147a15cecaf62
jhordyess <dev@jhordyess.com>
2023-06-27 17:10:45 +0000

    Added Prettier plugin for Tailwind CSS

--- history.sh
+++ history.sh
@@ -2,5 +2,6 @@ yarn create vite
 yarn install
 yarn add -D tailwindcss postcss autoprefixer
 yarn tailwindcss init -p
-yarn dev
 yarn add -D mathjs
+yarn add -D prettier prettier-plugin-tailwindcss
+yarn dev
