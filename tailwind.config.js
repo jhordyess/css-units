f0343902c642f2f793e5ceefb2754273736848d4
jhordyess <dev@jhordyess.com>
2023-06-28 01:33:39 +0000

    Added color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        classy: {
+          base: '#cad2c5',
+          primary: '#40493b',
+          secondary: '#9cb190',
+          background: '#b0d6e0',
+          accent: '#7b9fa9'
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
